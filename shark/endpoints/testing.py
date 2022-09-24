175a53783d771e11a21ec6f41dcaf75ec81afe61
Dekriel <92822789+Dekriel@users.noreply.github.com>
2022-09-24 20:17:20 +0100

    Black format

--- shark/endpoints/testing.py
+++ shark/endpoints/testing.py
@@ -7,6 +7,7 @@ router = APIRouter()
 async def test(request: Request):
     return {"message": "Wow, test succesful!"}
 
+
 @router.get("/test/{name}")
 async def test_name(request: Request, name: str):
     return {"message": f"Wow, test succesful! {name}"}
